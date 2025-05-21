bbe6ea1cad42a83e2a0826c798bc6bdc1c729bab
Krooths <higab6@gmail.com>
2025-05-21 11:09:55 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,5 @@
+
+// Código principal do seu projeto React/Next.js estará aqui.
+// Este é um placeholder. O código real foi desenvolvido no ChatGPT Canvas.
+console.log("Projeto Bruno – Desafio dos 7 Dias rodando!");
+    
